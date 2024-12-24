a8fae164988c50ea7ade7b57ca99d7a37ef4e5c9
farik <daouudasouleymanou@gmail.com>
2024-12-24 13:44:14 +0100

    Uploaded Solution

--- obj/Debug/net8.0/Create-a-Mini-Game-C-foundational-certification.AssemblyInfo.cs
+++ obj/Debug/net8.0/Create-a-Mini-Game-C-foundational-certification.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Create-a-Mini-Game-C-foundational-certification")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2bf8eda96df93998c91a12168a1bc86fff26e985")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ede5a3980297e5db21cc4d46ad8f64fdfb4f7e35")]
 [assembly: System.Reflection.AssemblyProductAttribute("Create-a-Mini-Game-C-foundational-certification")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Create-a-Mini-Game-C-foundational-certification")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
